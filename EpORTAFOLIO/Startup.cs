b305cb365d77f584c048386a300231b656fcd940
Fabiola <Fabiola@LAPTOP-5NFTL38M>
2023-03-30 18:23:36 -0700

    Add project files.

---
+++ EpORTAFOLIO/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EpORTAFOLIO.Startup))]
+namespace EpORTAFOLIO
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
